4e6d176bad0eb2540fee26aef931a312ef0f5f0b
michaelgeue <michael.geue@my.jcu.edu.au>
2020-03-17 11:38:39 +1000

    Refactor subject reader program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,24 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = get_data()
-    print(data)
+    subject_data = get_subject_data()
+    for subject in subject_data:
+        print("{:6} is taught by {:^12} and has {:3} students".format(subject[0], subject[1], subject[2]))
 
 
-def get_data():
+def get_subject_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
+    input_file = open("subject_data.txt", 'r')
+    subject_data = []
+
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject_data.append(parts)
+
     input_file.close()
+    return subject_data
 
 
 main()
