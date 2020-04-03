99799da1b5653454e81c41e0e3c6b27f696780dc
michaelgeue <michael.geue@my.jcu.edu.au>
2020-04-03 10:20:26 +1000

    Add Hex_Colours program

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,15 @@
+HEX_COLOURS = {"aliceblue": "#f0f8ff", "beige": "#f5f5dc", "black": "#000000", "cadetblue": "#5f9ea0",
+               "coral": "#ff7f50", "chocolate": "#d2691e", "forestgreen": "#228b22", "firebrick": "#b22222",
+               "goldenrod": "#daa520", "light": "#eedd82"}
+
+print(HEX_COLOURS)
+
+colour_choice = input("Enter colour: ").lower()
+
+while colour_choice != "":
+    if colour_choice in HEX_COLOURS:
+        print("{} is {}".format(colour_choice, HEX_COLOURS[colour_choice]))
+    else:
+        print("Invalid colour")
+    colour_choice = input("Enter colour: ").lower()
+
