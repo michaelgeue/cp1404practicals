b335dcca2b8e08c17c4928148050af4643057136
michaelgeue <michael.geue@my.jcu.edu.au>
2020-04-03 10:05:28 +1000

    Refactor state names program

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,17 +5,17 @@ File needs reformatting
 """
 
 # Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+STATE_DICT = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+              "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+print(STATE_DICT)
+short_state_code = input("Enter short state: ").upper()
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+while short_state_code != "":
+    if short_state_code in STATE_DICT:
+        print(short_state_code, "is", STATE_DICT[short_state_code])
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
+    short_state_code = input("Enter short state: ").upper()
 
-for state in CODE_TO_NAME:
-    print("{:3} is {}".format(state, CODE_TO_NAME[state]))
+for state in STATE_DICT:
+    print("{:3} is {}".format(state, STATE_DICT[state]))
