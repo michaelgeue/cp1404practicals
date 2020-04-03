039fb114da1ee56435b0873bd4e34aacaa8a3deb
michaelgeue <michael.geue@my.jcu.edu.au>
2020-04-03 10:23:43 +1000

    Add functions to Hex_Colours and state_names

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,14 +2,18 @@ HEX_COLOURS = {"aliceblue": "#f0f8ff", "beige": "#f5f5dc", "black": "#000000", "
                "coral": "#ff7f50", "chocolate": "#d2691e", "forestgreen": "#228b22", "firebrick": "#b22222",
                "goldenrod": "#daa520", "light": "#eedd82"}
 
-print(HEX_COLOURS)
 
-colour_choice = input("Enter colour: ").lower()
+def main():
+    print(HEX_COLOURS)
 
-while colour_choice != "":
-    if colour_choice in HEX_COLOURS:
-        print("{} is {}".format(colour_choice, HEX_COLOURS[colour_choice]))
-    else:
-        print("Invalid colour")
     colour_choice = input("Enter colour: ").lower()
 
+    while colour_choice != "":
+        if colour_choice in HEX_COLOURS:
+            print("{} is {}".format(colour_choice, HEX_COLOURS[colour_choice]))
+        else:
+            print("Invalid colour")
+        colour_choice = input("Enter colour: ").lower()
+
+
+main()
--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -7,15 +7,21 @@ File needs reformatting
 # Reformat this file so the dictionary code follows PEP 8 convention
 STATE_DICT = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(STATE_DICT)
-short_state_code = input("Enter short state: ").upper()
 
-while short_state_code != "":
-    if short_state_code in STATE_DICT:
-        print("{} is {}".format(short_state_code, STATE_DICT[short_state_code]))
-    else:
-        print("Invalid short state")
+
+def main():
+    print(STATE_DICT)
     short_state_code = input("Enter short state: ").upper()
 
-for state_name_short, state_name_full in STATE_DICT.items():
-    print("{:3} is {}".format(state_name_short, state_name_full))
+    while short_state_code != "":
+        if short_state_code in STATE_DICT:
+            print("{} is {}".format(short_state_code, STATE_DICT[short_state_code]))
+        else:
+            print("Invalid short state")
+        short_state_code = input("Enter short state: ").upper()
+
+    for state_name_short, state_name_full in STATE_DICT.items():
+        print("{:3} is {}".format(state_name_short, state_name_full))
+
+
+main()
