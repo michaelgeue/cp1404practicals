78ca4256627e63424f0537b4a0306603709faaf0
michaelgeue <michael.geue@my.jcu.edu.au>
2020-04-09 19:38:54 +1000

    Add languages and programming language program

---
+++ prac_06/languages.py
@@ -0,0 +1,23 @@
+"""Languages Practical Program by Michael Geue"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(ruby)
+    print(python)
+    print(visual_basic)
+
+    programming_languages = [ruby, python, visual_basic]
+    print("The dynamically typed languages are:")
+
+    for language in programming_languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
