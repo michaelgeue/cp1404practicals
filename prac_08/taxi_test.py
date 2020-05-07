24eb3bd54d6fd3524c90373779da4fd7d9eee997
michaelgeue <michael.geue@my.jcu.edu.au>
2020-05-07 23:59:02 +1000

    Add tests for getting fare cost

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -7,9 +7,11 @@ def main():
     prius_1 = Taxi('Prius 1', 100)
     prius_1.drive(40)
     print(prius_1)
+    print("Current Fare: ${:.2f}".format(prius_1.get_fare()))
     prius_1.start_fare()
     prius_1.drive(100)
     print(prius_1)
+    print("Current Fare: ${:.2f}".format(prius_1.get_fare()))
 
 
 if __name__ == '__main__':
