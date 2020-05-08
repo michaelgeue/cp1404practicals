0e7e35e541a962875c5330d962e648f569c355ec
michaelgeue <michael.geue@my.jcu.edu.au>
2020-05-08 23:09:33 +1000

    Add visual counter to unreliable car test

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -6,9 +6,14 @@ from prac_08.unreliable_car import UnreliableCar
 def main():
     okay_car = UnreliableCar("Okay Car", 200, 50)
     print(okay_car)
+    drive_count = 0
+
     while okay_car.fuel != 0:
         okay_car.drive(50)
         print(okay_car)
+        drive_count += 1
+
+    print("Times driven: {}".format(drive_count))
 
 
 if __name__ == '__main__':
